37d2ee67244f30fbd824f0477ccabbcc1fe759b9
9deok <butterflynine20@gmail.com>
2022-01-02 22:51:06 +0900

    spring actuator

--- src/main/java/prac/webflux/InventoryService.java
+++ src/main/java/prac/webflux/InventoryService.java
@@ -5,14 +5,26 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class InventoryService {
 
     private ItemRepository itemRepository;
 
-    public InventoryService(ItemRepository itemRepository) {
+    private CartRepository cartRepository;
+
+    public InventoryService(ItemRepository itemRepository, CartRepository cartRepository) {
         this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
+    }
+
+    public Mono<Cart> getCart(String cartId) {
+        return this.cartRepository.findById(cartId);
+    }
+
+    public Flux<Item> getInventory() {
+        return this.itemRepository.findAll();
     }
 
     Flux<Item> searchByEaxmple(String name, String description, boolean useAnd) {
@@ -28,5 +40,26 @@ public class InventoryService {
         Example<Item> probe = Example.of(item,matcher);
 
         return itemRepository.findAll(probe);
+    }
+
+    Mono<Cart> addItemToCart(String cartId, String itemId) {
+        return this.cartRepository.findById(cartId)
+            .defaultIfEmpty(new Cart(cartId))
+            .flatMap(cart -> cart.getCartItems().stream()
+                .filter(cartItem -> cartItem.getItem().getId().equals(itemId))
+                .findAny()
+                .map(cartItem -> {
+                    cartItem.increment();
+                    return Mono.just(cart);
+                })
+                .orElseGet(() -> {
+                    return this.itemRepository.findById(itemId)
+                        .map(item -> new CartItem(item))
+                        .map(cartItem -> {
+                            cart.getCartItems().add(cartItem);
+                            return cart;
+                        });
+                }))
+            .flatMap(cart -> this.cartRepository.save(cart));
     }
 }
--- src/main/java/prac/webflux/Item.java
+++ src/main/java/prac/webflux/Item.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.geo.Point;
 
-@Getter
-@Setter
 public class Item {
 
     private @Id String id;
@@ -27,6 +25,83 @@ public class Item {
         this.name = name;
         this.description = description;
         this.price = price;
+    }
+
+    public Item(String id, String name, String description, double price) {
+        this(name,description,price);
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDistributorRegion() {
+        return distributorRegion;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getAvailableUnits() {
+        return availableUnits;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDistributorRegion(String distributorRegion) {
+        this.distributorRegion = distributorRegion;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setAvailableUnits(int availableUnits) {
+        this.availableUnits = availableUnits;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     @Override
